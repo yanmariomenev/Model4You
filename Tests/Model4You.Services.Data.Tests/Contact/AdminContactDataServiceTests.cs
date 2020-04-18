6ff6539d3c3745701899486fb51c29305907a902
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2020-04-18 17:59:43 +0300

    Add one test to adminContactService
    MoveToAnswered;
    The two services are covered. Might need to look into them again.

--- Tests/Model4You.Services.Data.Tests/Contact/AdminContactDataServiceTests.cs
+++ Tests/Model4You.Services.Data.Tests/Contact/AdminContactDataServiceTests.cs
@@ -88,6 +88,29 @@ namespace Model4You.Services.Data.Tests.Contact
             Assert.Equal(0, count);
         }
 
+        [Fact]
+        public async Task MoveToAnswered_ShouldMoveUnAnsweredQuestionToAnswered()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var repository = new EfDeletableEntityRepository<ContactFormData>(new ApplicationDbContext(options));
+
+            var service = new ContactDataService(repository);
+
+
+            var form = await this.CreateContactForm("Pesho", "Pesho@abv.bg", "Test", "Test", true, repository);
+            var form2 = await this.CreateContactForm("Pesho2", "Pesho2@abv.bg", "Test2", "Test2", false, repository);
+            var form3 = await this.CreateContactForm("Pesho2", "Pesho2@abv.bg", "Test2", "Test2", false, repository);
+            var formIdToString = form2.ToString();
+
+            await service.MoveToAnswered(formIdToString);
+            var check = await repository.AllWithDeleted().Where(x => x.Id == form2 && x.IsDeleted).FirstOrDefaultAsync();
+
+            Assert.NotNull(check);
+        }
+
+
         private async Task<int> CreateContactForm
             (string name, string email, string subject, string message, bool answeredOrNot, IDeletableEntityRepository<ContactFormData> repo)
         {
