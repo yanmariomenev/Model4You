ae50c4d191c672860739a9d7f16435133d55e6e6
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2020-04-18 17:48:37 +0300

    Add contact form tests .
    Create;
    Admin :
    TakeAllUnAnswered;
     TakeAllUnAnswered;
    Covered :white_check_mark:

---
+++ Tests/Model4You.Services.Data.Tests/Contact/AdminContactDataServiceTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Model4You.Data;
+using Model4You.Data.Common.Repositories;
+using Model4You.Data.Models;
+using Model4You.Data.Repositories;
+using Model4You.Services.Data.AdminServices;
+using Model4You.Web.ViewModels.Administration.Dashboard;
+using Xunit;
+
+namespace Model4You.Services.Data.Tests.Contact
+{
+    public class AdminContactDataServiceTests : BaseServiceTest
+    {
+        [Fact]
+        public async Task TakeAllUnAnswered_ShouldReturnAllUnAnsweredQuestions()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var repository = new EfDeletableEntityRepository<ContactFormData>(new ApplicationDbContext(options));
+
+            var service = new ContactDataService(repository);
+
+
+            var form = await this.CreateContactForm("Pesho", "Pesho@abv.bg", "Test", "Test",true, repository);
+            var form2 = await this.CreateContactForm("Pesho2", "Pesho2@abv.bg", "Test2", "Test2",false, repository);
+            var form3 = await this.CreateContactForm("Pesho2", "Pesho2@abv.bg", "Test2", "Test2", false, repository);
+
+            var takeAll = await service.TakeAllUnAnswered<ContactFormDataView>();
+            var count = takeAll.Count();
+
+            Assert.Equal(2, count);
+        }
+
+        [Fact]
+        public async Task TakeAllUnAnswered_WhenThereIsNon_ShouldReturnZero()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var repository = new EfDeletableEntityRepository<ContactFormData>(new ApplicationDbContext(options));
+
+            var service = new ContactDataService(repository);
+
+            var takeAll = await service.TakeAllUnAnswered<ContactFormDataView>();
+            var count = takeAll.Count();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public async Task TakeAllAnswered_ShouldReturnAllUnAnsweredQuestions()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var repository = new EfDeletableEntityRepository<ContactFormData>(new ApplicationDbContext(options));
+
+            var service = new ContactDataService(repository);
+
+
+            var form = await this.CreateContactForm("Pesho", "Pesho@abv.bg", "Test", "Test", true, repository);
+            var form2 = await this.CreateContactForm("Pesho2", "Pesho2@abv.bg", "Test2", "Test2", false, repository);
+            var form3 = await this.CreateContactForm("Pesho2", "Pesho2@abv.bg", "Test2", "Test2", false, repository);
+
+            var takeAll = await service.TakeAllAnswered<ContactFormDataView>();
+            var count = takeAll.Count();
+
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public async Task TakeAllAnswered_WhenThereIsNon_ShouldReturnZero()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var repository = new EfDeletableEntityRepository<ContactFormData>(new ApplicationDbContext(options));
+
+            var service = new ContactDataService(repository);
+
+            var takeAll = await service.TakeAllUnAnswered<ContactFormDataView>();
+            var count = takeAll.Count();
+
+            Assert.Equal(0, count);
+        }
+
+        private async Task<int> CreateContactForm
+            (string name, string email, string subject, string message, bool answeredOrNot, IDeletableEntityRepository<ContactFormData> repo)
+        {
+            var contact = new ContactFormData
+            {
+                Name = name,
+                Email = email,
+                Subject = subject,
+                Message = message,
+                IsDeleted = answeredOrNot,
+            };
+            await repo.AddAsync(contact);
+            await repo.SaveChangesAsync();
+            return contact.Id;
+        }
+    }
+}
---
+++ Tests/Model4You.Services.Data.Tests/Contact/ContactServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Model4You.Data;
+using Model4You.Data.Common.Repositories;
+using Model4You.Data.Models;
+using Model4You.Data.Repositories;
+using Xunit;
+
+namespace Model4You.Services.Data.Tests.Contact
+{
+    public class ContactServiceTests : BaseServiceTest
+    {
+        [Fact]
+        public async Task Create_ShouldCreateForm()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var repository = new EfDeletableEntityRepository<ContactFormData>(new ApplicationDbContext(options));
+
+            var service = new ContactFormService.ContactFormService(repository);
+
+
+            //var form = await this.CreateContactForm("Pesho", "Pesho@abv.bg", "Test","Test", repository);
+            //var form2 = await this.CreateContactForm("Pesho2", "Pesho2@abv.bg", "Test2", "Test2", repository);
+            await service.Create("Pesho2", "Pesho2@abv.bg", "Test2", "Test2");
+            var count = await repository.All().CountAsync();
+
+            Assert.Equal(1, count);
+
+        }
+
+        private async Task<int> CreateContactForm
+            (string name, string email, string subject, string message, IDeletableEntityRepository<ContactFormData> repo)
+        {
+            var contact = new ContactFormData
+            {
+                Name = name,
+                Email = email,
+                Subject = subject,
+                Message = message,
+            };
+            await repo.AddAsync(contact);
+            await repo.SaveChangesAsync();
+            return contact.Id;
+        }
+    }
+}
